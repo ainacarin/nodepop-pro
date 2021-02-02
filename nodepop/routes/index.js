16e9fdcafb9f5d00a2f235c8981e383fe3028d71
Aida N <nacarinobazaga@gmail.com>
2021-02-02 10:35:33 +0100

    Añado para mostrar anuncios sin filtros sin paginación, no muestra lista de tags

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const Advertisement = require('../models/Advertisement');
+const Tag = require('../models/Tag');
+
+const imagesPath = 'images/';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  
+  try {
+    res.locals.title = 'NODEPOP';
+    
+    const advertisementsResults = await Advertisement.find(); 
+
+    advertisementsResults.forEach(advertisement => {
+      if(advertisement.image) { 
+        advertisement.image = imagesPath.concat(advertisement.image);
+      }
+    });
+    
+    res.locals.advertisements = advertisementsResults;
+    
+    res.render('index');
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
