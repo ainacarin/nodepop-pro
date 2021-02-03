6ba08f7e63b2aa474692a6771c90e7f050a0ceb0
Aida N <nacarinobazaga@gmail.com>
2021-02-03 12:09:08 +0100

    Añade validadores en modelo Advertisement

--- nodepop/models/Advertisement.js
+++ nodepop/models/Advertisement.js
@@ -2,14 +2,41 @@
 
 const mongoose = require("mongoose");
 
+const tagsValues = ["work", "lifestyle", "motor", "mobile"];
+
 //Schema
 const advertisementSchema = mongoose.Schema(
   {
-    name: { type: String, index: true },
-    sale: { type: Boolean, index: true },
-    price: { type: Number, index: true },
+    name: {
+      type: String,
+      index: true,
+      required: [true, "Advertisement name is required"],
+    },
+    sale: {
+      type: Boolean,
+      index: true,
+      required: [
+        true,
+        "Advertisement type sale is required. Sale = true, Not sale = false",
+      ],
+    },
+    price: {
+      type: Number,
+      index: true,
+      required: [true, "Advertisement price is required.  Format = XXXXX.XX"],
+    },
     image: String,
-    tags: [String],
+    // tags: [String],
+    tags: {
+      type: [String],
+      validate: {
+        validator: function (v) {
+          const filtered = tagsValues.filter(tagValue => v.includes(tagValue));
+          return filtered.length === v.length;
+        },
+        message: "Tags only can be: work, lifestyle, mortor and/or mobile",
+      },
+    },
   },
   {
     collection: "advertisements",
@@ -26,24 +53,24 @@ advertisementSchema.statics.list = function (queryData) {
     }
   }
 
-  if(queryData.name) {
-    filter.name = { $regex: "^"+queryData.name, $options: 'm' };
-    console.log(filter.name)
+  if (queryData.name) {
+    filter.name = { $regex: "^" + queryData.name, $options: "m" };
+    console.log(filter.name);
   }
-  
-  if(queryData.sale) {
+
+  if (queryData.sale) {
     filter.sale = queryData.sale;
   }
 
-  if(queryData.min) {
+  if (queryData.min) {
     filter.price = { $gte: queryData.min };
-    if(queryData.max) {
+    if (queryData.max) {
       filter.price = { $gte: queryData.min, $lte: queryData.max };
     }
-  } else if(queryData.max) {
+  } else if (queryData.max) {
     filter.price = { $lte: queryData.max };
   }
-  
+
   const limit = parseInt(queryData.limit);
   const skip = parseInt(queryData.skip);
 
